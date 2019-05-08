5eaa87c6c6ab5061b7988f4291dbc8b9bd7315e0
AdmiringWorm <kim.nordmo@gmail.com>
2019-05-08 21:11:19 +0200

    (maint) Created configuration class to hold common addin values
    This is a class that will be used in cooperation with
    setting and getting the location of warp, along with
    other common values only used inside the addin

--- src/Cake.Warp/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Cake.Warp
-{
-    public class Class1
-    {
-    }
-}
---
+++ src/Cake.Warp/Common/AddinConfiguration.cs
@@ -0,0 +1,63 @@
+namespace Cake.Warp.Common
+{
+    using System;
+    using System.Runtime.InteropServices;
+
+    internal sealed class AddinConfiguration
+    {
+        private static AddinConfiguration instance;
+
+        private AddinConfiguration()
+        {
+        }
+
+        /// <summary>
+        ///     Gets the singleton instance of the addin configuration class
+        /// </summary>
+        public static AddinConfiguration Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new AddinConfiguration();
+                }
+
+                return instance;
+            }
+        }
+
+        /// <summary>
+        ///   Gets the current path to the executing assembly.
+        /// </summary>
+        public string AssemblyPath { get; }
+
+        /// <summary>
+        ///   Gets or sets the full file path to the warp binary file.
+        /// </summary>
+        /// <notes>
+        ///   This should/will be set in the
+        ///   <see cref="ModuleInitializer.Initialize" />
+        ///   during library loading.
+        /// </notes>
+        public string WarpFilePath { get; set; }
+
+        /// <summary>
+        ///     Gets wether we are running on the Linux platform or not.
+        /// </summary>
+        public bool IsLinux { get; }
+            = RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
+
+        /// <summary>
+        ///     Gets wether we are running on the Mac OSX platform or not.
+        /// </summary>
+        public bool IsMacOS { get; }
+            = RuntimeInformation.IsOSPlatform(OSPlatform.OSX);
+
+        /// <summary>
+        ///     Gets wether we are running on the Windows platform or not.
+        /// </summary>
+        public bool IsWindows { get; }
+            = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+    }
+}

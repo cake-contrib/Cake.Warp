cfeeb3244526d4fa1548e5bcbd9643071a9d21e9
AdmiringWorm <kim.nordmo@gmail.com>
2019-05-10 16:43:10 +0200

    (GH-7) Created basic settings class for warp
    fixes #7

---
+++ src/Cake.Warp/WarpPlatforms.cs
@@ -0,0 +1,24 @@
+namespace Cake.Warp
+{
+    /// <summary>
+    /// The platform to use when creating
+    /// the self-contained application.
+    /// </summary>
+    public enum WarpPlatforms
+    {
+        /// <summary>
+        /// Create the application as a Linux x64 executable.
+        /// </summary>
+        LinuxX64,
+
+        /// <summary>
+        /// Create the application as a Mac OSX x64 executable.
+        /// /// </summary>
+        MacOSX64,
+
+        /// <summary>
+        /// Create the application as a Windows x64 executable.
+        /// </summary>
+        WindowsX64,
+    }
+}
---
+++ src/Cake.Warp/WarpSettings.cs
@@ -0,0 +1,63 @@
+namespace Cake.Warp
+{
+    using Cake.Core.IO;
+    using Cake.Core.Tooling;
+    // using Cake.Warp.Common;
+
+    /// <summary>
+    /// Contains the settings used by <see cref="WarpRunner" />.
+    /// </summary>
+    public sealed class WarpSettings : ToolSettings
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WarpSettings" /> class,
+        /// with ToolPath set to the extracted warp binary file.
+        /// </summary>
+        /// <remarks>ToolPath can be overridden if desired.</remarks>
+        public WarpSettings()
+        {
+            // We can not enable this until PR #11 is merged
+            // this.ToolPath = AddinConfiguration.Instance.WarpFilePath;
+        }
+
+        /// <summary>
+        /// Gets or sets the platform and architecture to create the
+        /// self-contained application for.
+        /// </summary>
+        public WarpPlatforms Architecture { get; set; }
+
+        /// <summary>
+        /// Gets or sets the platform and architecture to create the
+        /// self-contained application for.
+        /// </summary>
+        /// <remarks>
+        /// This is just an alias for <see cref="Architecture" />.
+        /// </remarks>
+        public WarpPlatforms Platform
+        {
+            get => this.Architecture;
+            set => this.Architecture = value;
+        }
+
+        /// <summary>
+        /// Gets or sets the name of the executable to run after the
+        /// the created application is being executed by a user.
+        /// </summary>
+        /// <remarks>
+        /// This should only be the filaname including the file extension.
+        /// </remarks>
+        public string ExecutableName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the directory that will be packed into the
+        /// self-contained application.
+        /// </summary>
+        public DirectoryPath InputDirectory { get; set; }
+
+        /// <summary>
+        /// Gets or sets the output path to where the self-contained application
+        /// should be created.
+        /// </summary>
+        public FilePath OutputFilePath { get; set; }
+    }
+}
